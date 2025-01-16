dac7ee33012336fe2ca98f0c3175223ea161b171
Ishan Kc <ishankc777@gmail.com>
2025-01-16 09:44:28 +0545

    revised

---
+++ frontend/forgot__pw.js
@@ -0,0 +1,10 @@
+function data(){
+    var a = document.getElementById("n1").value;
+
+    if(a == ""){
+        alert("All Fields are Mandatory");
+        return false;
+    } else {
+        true;
+    }
+}
--- frontend/login.js
+++ frontend/login.js
@@ -10,3 +10,29 @@ registerBtn.addEventListener('click', () =>{
 loginBtn.addEventListener('click', () =>{
     container.classList.remove("active");
 });
+
+//form validation
+function data__signin(){
+    var a = document.getElementById("n1").value;
+    var b = document.getElementById("n2").value;
+    if(a=="" || b==""){
+        alert("All Fields are Mandatory");
+        return false;
+    }else{
+        true;
+    }
+
+}
+function data__signup(){
+    var a = document.getElementById("n3").value;
+    var b = document.getElementById("n4").value;
+    var c = document.getElementById("n5").value;
+    var d = document.getElementById("n6").value;
+
+    if(a=="" || b=="" || c=="" || d==""){
+            alert("All Fields are Mandatory");
+            return false;
+    } else {
+        true;
+    } 
+}
